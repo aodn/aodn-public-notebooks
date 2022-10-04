008afcb7623f45a91152ce1c941792aec7ae14e2
Alex Leith <alexgleith@gmail.com>
2022-10-04 16:28:12 +1100

    Add SST creation using MF Dataset

---
+++ zarr_creation/sst_zarr_creation_mfdataset.py
@@ -0,0 +1,65 @@
+import s3fs
+import xarray as xr
+from toolz import partition
+
+
+CONSISTENT_VARS = {
+    "time",
+    "lat",
+    "lon",
+    "dt_analysis",
+    "l2p_flags",
+    "quality_level",
+    "satellite_zenith_angle",
+    "sea_surface_temperature",
+    "sses_bias",
+    "sses_count",
+    "sses_standard_deviation",
+    "sst_dtime",
+}
+
+
+def get_files(source_path, s3):
+    glob_result = s3.glob(source_path)
+
+    return glob_result
+
+
+def main(source_path, store_path):
+    s3 = s3fs.S3FileSystem(anon=False)
+    files = get_files(source_path, s3)
+
+    print(f"Found {len(files)} files to process")
+
+    first_file = True
+    completed = 0
+
+    def drop_vars(dataset):
+        available_vars = set(dataset.variables)
+        to_drop_vars = list(available_vars - CONSISTENT_VARS)
+        return dataset.drop_vars(to_drop_vars)
+
+    for files_subset in partition(10, files):
+        paths = [s3.open(file) for file in files_subset]
+        ds = xr.open_mfdataset(
+            paths,
+            engine="h5netcdf",
+            chunks={"time": 10, "lon": 1000, "lat": 1000},
+            preprocess=drop_vars,
+        )
+
+        if first_file:
+            ds.to_zarr(store_path, mode="w", consolidated=True)
+            first_file = False
+        else:
+            ds.to_zarr(store_path, mode="a", append_dim="time", consolidated=True)
+
+        completed += 10
+        print(f"Finished {completed} out of {len(files)}")
+
+
+if __name__ == "__main__":
+    source_path = "s3://imos-data/IMOS/SRS/SST/ghrsst/L3S-1d/day/2022/*.nc"
+    store_path = "s3://imos-data-pixeldrill/alexl/sst_mfd.zarr"
+
+    main(source_path, store_path)
